71428e12d7bdb415ff68afe276b521aa0c8ee1b3
ehsanYaghoti <ehsan.yaghoti@yahoo.com>
2024-12-27 17:27:47 +0330

    remove login check in auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,19 +1,8 @@
 'use client';
 
-import callApi from "@/helpers/callApi";
-import checkAuth from "@/helpers/checkAuth";
-import { CircularProgress } from "@mui/material";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-
-
 export default function AuthLayout({children } : {children: React.ReactNode}) {
   
     // check user is authenticated if so then redirect to user panel (or home route) then be toastified.
-  
-    
-
   
     return (
             <main className="w-full h-full p-6 min-h-screen flex items-center justify-center" >
